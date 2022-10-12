77ccd83b31ed41558f7e49b1c83608a1c3990410
Grzegorz Pacewicz <g.pacewicz@gmail.com>
2022-10-12 07:13:13 +0200

    Rebuliding js functions.

--- Lekcja 6/ToDo-List-Js/js/script.js
+++ Lekcja 6/ToDo-List-Js/js/script.js
@@ -1,38 +1,55 @@
 {
+  const tasks = [
+    {
+      content: "nagrać lekcje",
+      done: false,
+    },
+    {
+      content: "zjeść pierogi",
+      done: true,
+    },
+  ];
 
-    const tasks = [
-        {
-            content: "nagrać lekcje",
-            done: false,
-        },
-        {
-            content: "zjeść pierogi",
-            done: true,
-        },
+  const render = () => {
+    let htmlString = "";
 
-    ];
-
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString += `
-            <li> 
+    for (const task of tasks) {
+      htmlString += `
+            <li
+            ${task.done ? ' style="text-decoration: line-through"' : ""}> 
+            
             ${task.content}
             </li>
         `;
-
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
     }
 
-    const init = () => {
-        render();
+    document.querySelector(".js-tasks").innerHTML = htmlString;
+  };
 
-    };
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
 
-    init();
+    render();
+  };
 
+  const init = () => {
+    render();
 
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+
+      const newTaskContent = document.querySelector(".js-newTask").value.trim();
+      if (newTaskContent === "") {
+        return;
+      }
+
+      addNewTask(newTaskContent);
+    });
+  };
+
+  init();
 }
